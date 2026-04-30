8e79f729b705be007b69420ef4bc0071ae1b5b42
Michael Büsch <m@bues.ch>
2026-04-30 18:09:58 +0200

    Add install scripts

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+basedir="$(realpath "$0" | xargs dirname)"
+
+. "$basedir/scripts/lib.sh"
+
+install_entry_checks()
+{
+    [ -d "$target" ] || die "codepal is not built! Run ./build.sh"
+    [ "$(id -u)" = "0" ] || die "Must be root to install codepal."
+}
+
+install_dirs()
+{
+    do_install \
+        -o root -g root -m 0755 \
+        -d /opt/codepal/bin
+
+    do_install \
+        -o root -g root -m 0755 \
+        -d /opt/codepal/etc
+
+    do_install \
+        -o root -g root -m 0755 \
+        -d /opt/codepal/etc/codepal
+}
+
+install_codepal()
+{
+    do_install \
+        -o root -g root -m 0755 \
+        "$target/codepal" \
+        /opt/codepal/bin/
+}
+
+release="release"
+while [ $# -ge 1 ]; do
+    case "$1" in
+        --debug|-d)
+            release="debug"
+            ;;
+        --release|-r)
+            release="release"
+            ;;
+        *)
+            die "Invalid option: $1"
+            ;;
+    esac
+    shift
+done
+target="$basedir/target/$release"
+
+install_entry_checks client
+install_dirs
+install_codepal
